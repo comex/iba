c119b6f3ceeb462eec53d0b33c6297393410e429
comex <comexk@gmail.com>
2009-09-21 16:54:46 -0400

    unification

--- agi.py
+++ agi.py
@@ -1,12 +1,6 @@
 import re, sys, os, shutil, email.Utils, datetime
 #!/usr/bin/env python
 # Note: time.py is a really bad name
-vr = sys.stdin.read()
-lines = vr.split('\n')
-dt = [lines[i+1] for i in xrange(len(lines)) if lines[i].find('by yzma.clarkk.net') != -1 or lines[i].find('by pzk37') != -1][0]
-
-now = datetime.datetime.utcfromtimestamp(email.Utils.mktime_tz(email.Utils.parsedate_tz(dt[dt.index(';')+2:])))
-
 def wrap(text, width):
     #http://code.activestate.com/recipes/148061/
     return reduce(lambda line, word, width=width: '%s%s%s' %
@@ -61,135 +55,43 @@ abbrevs = {
     'Medal': 'Medal',
 }
 abbrevs2 =  dict(((v, k) for k, v in abbrevs.items()))
-DELIM = '\n\n===============================================================================\n\n'
-iba = open('iba.txt').read().strip()
-sections = iba.split(DELIM)
 
-ox = int('Offers' in sections[2])
+class iba_report:
+    DELIM = '\n\n===============================================================================\n\n'
+    prev2rate = [1.00, 1.00, 1.00, 1.00, 0.90, 0.90, 0.90, 0.80, 0.80, 0.80, 0.73, 0.62, 0.50, 0.38, 0.26, 0.18, 0.12, 0.08, 0.05, 0.03, 0.01]
+    def __init__(self, name='iba.txt'):
+        iba = open(name).read().strip()
+        self.sections = iba.split(self.DELIM)
+        self.ox = int('Offers' in self.sections[2])
+        self.totals = []
 
-holdings = {}
-ibap = []
-for hol in sections[1].split('\n'):
-    if '      ' in hol and 'Nickname' not in hol:
-        person, zm = re.split('  +', hol)
-        if person[0] == '*':
-            person = person[1:]
-            ibap.append(person)
-        holdings[person] = int(zm)
+    def read_all(self):
+        self.read_holdings()
+        self.read_rates()
+        self.read_history()
+        self.read_contract()
 
-rates = []
-for rate in sections[2+ox].split('\n'):
-    if re.search('[0-9]', rate):
-        r = re.split('   +', rate)
-        r = map(str.strip, (rate[:16], rate[16:30], rate[30:]))
-        r = [r[0], int(r[1]), int(r[2]) if r[2] != '' else 0]
-        rates.append(r)
-    else:
-        rates.append(rate)
+    def write_all():
+        self.write_holdings()
+        self.write_rates()
+        self.write_history()
 
-def lookupRate(x, damt=None):
-    if x == 'Drop your Weapon': x = 'Drop your Wea..'
-    for a in rates:
-        if type(a) == list and a[0] == x:
-            if damt is not None:
-                a[2] = a[2] + damt
-                assert a[2] >= 0
-            return a[1]
-    raise KeyError(x)
+    def read_holdings(self):
+        holdings = {}
+        ibap = []
+        for hol in self.sections[1].split('\n'):
+            if '      ' in hol and 'Nickname' not in hol:
+                person, zm = re.split('  +', hol)
+                if person[0] == '*':
+                    person = person[1:]
+                    ibap.append(person)
+                holdings[person] = int(zm)
+        self.holdings = holdings
+        self.ibap = ibap
 
-history = sections[3+ox]
-
-monday = datetime.datetime.utcnow()
-monday = (monday - datetime.timedelta(days=monday.weekday())).replace(hour=0, minute=0, second=0, microsecond=0)
-
-datefmt = '%d %B %Y %H:%M:%S'
-hist = ''
-when = None
-for line in history.split('\n'):
-    if line.startswith('*'): continue
-    a, b = line[:27].strip(), line[27:].strip()
-    if a != '' and a != 'History:' and '[' not in a:
-        when = datetime.datetime.strptime(a, datefmt)
-    if b != '' and when >= monday:
-        hist += b + '\n' 
-hist = hist.replace(',\n', ', ')
-hist = hist.replace(' +', '\n+')
-hist = hist.replace(' -', '\n-')
-prev = {}
-for line in hist.split('\n'):
-    line = line.strip()
-    if line == '' or '[' in line or ']' in line: continue
-    if line[0] == '+':
-        m = re.search('\((.*)\)', line)
-        if not m: continue
-        for n in m.group(1).split(','):
-            n = n.strip().split('*')
-            if len(n) > 1:
-                p = int(n[0])
-            else:
-                p = 1
-            prev[actor] = prev.get(actor, 0) + p
-
-    else:
-        actor = line
-
-prev2rate = [1.00, 1.00, 1.00, 1.00, 0.90, 0.90, 0.90, 0.80, 0.80, 0.80, 0.73, 0.62, 0.50, 0.38, 0.26, 0.18, 0.12, 0.08, 0.05, 0.03, 0.01]
-
-person = raw_input('Person> ')
-sores = []
-totals = []
-def transact(typ, asts, sorted_mode=None): # typ is 'deposit'/'withdraw'
-    total = 0
-    sore = []
-    stuff = []
-    for ast in asts.split(','):
-        ii = 1
-        if '*' in ast:
-            ast = ast.split('*')
-            ii, ast = int(ast[0]), ast[1].strip()
-        stuff.append((ii, ast))
-    for ii, ast in (sorted(stuff, key=lambda (a, b): b) if sorted_mode else stuff):
-        if ii > 1:
-            sore.append('%d*%s' % (ii, abbrevs.get(ast, ast)))
-        else:
-            sore.append(abbrevs.get(ast, ast))
-        ast = abbrevs2.get(ast, ast)
-        for i in xrange(ii):
-            if typ == 'deposit':
-                p = prev.get(person, 0)
-                if p >= 21:
-                    rmul = 0
-                else:
-                    rmul = prev2rate[p]
-                prev[person] = p + 1
-            else:
-                rmul = -1
-            erate = round(rmul * lookupRate(ast, 1 if typ == 'deposit' else -1))
-            holdings[person] = holdings.get(person, 0) + int(erate)
-            total += int(erate)
-            if holdings[person] < 0: raise ValueError(holdings[person])
-    total = ('+' * (typ == 'deposit')) + str(total) + 'zm'
-    totals.append((total, sore))
-
-while True:
-    line = raw_input()
-    if line == '': break
-    transact(*line.strip().split())
-
-max_total_length = max(len(a[0]) for a in totals)
-for total, sore in totals:
-    total = (' ' * (max_total_length - len(total))) + total
-    b = 28 + len(person) + 1 + len(total) + 2
-    c = ' ' * b
-    sores.append(total + ' (' + wrap(', '.join(sore), 70 - b).replace('\n', '\n' + c) + ')')
-
-
-if len(sores) > 0:
-    hdr = now.strftime(datefmt).ljust(27) + person + ' '
-    hdr2 = '\n' + ' ' * len(hdr)
-    history += '\n' + hdr + hdr2.join(sores)
-
-sections[1] = '''Current Holdings:
+    def write_holdings(self):
+        self.sections[1] = '''
+Current Holdings:
 
 Nickname                     zm
 ---------------------------------
@@ -197,11 +99,182 @@ Nickname                     zm
 
 * IBA party
 
-All IBA parties are listed.  All other persons have no zm.''' % '\n'.join( (('*' if person in ibap else '') + person).ljust(29) + str(zm) for person, zm in sorted(holdings.items(), key=lambda a: a[0].lower()))
+All IBA parties are listed.  All other persons have no zm.
+        '''.strip() % '\n'.join( (('*' if person in self.ibap else '') + person).ljust(29) + str(zm) for person, zm in sorted(self.holdings.items(), key=lambda a: a[0].lower()))
 
-sections[2+ox] = '\n'.join(i if isinstance(i, basestring) else (i[0].ljust(16) + str(i[1]).ljust(14) + (str(i[2]) * (i[2] != 0))).rstrip() for i in rates)
+    def read_rates(self):
+        rates = []
+        rdict = {}
+        for rate in self.sections[2+self.ox].split('\n'):
+            if re.search('[0-9]', rate):
+                r = re.split('   +', rate)
+                r = map(str.strip, (rate[:16], rate[16:30], rate[30:]))
+                r = [r[0], int(r[1]), int(r[2]) if r[2] != '' else 0]
+                rates.append(r)
+                rdict[r[0]] = r[1]
+            else:
+                rates.append(rate)
+        self.rates = rates
+        self.rdict = rdict
+    
+    def write_rates(self):
+        self.sections[2+self.ox] = '\n'.join(i if isinstance(i, basestring) else (i[0].ljust(16) + str(i[1]).ljust(14) + (str(i[2]) * (i[2] != 0))).rstrip() for i in self.rates)
 
-sections[3+ox] = history
+    def lookup_rate(self, x, change_amt=0):
+        if x == 'Drop your Weapon': x = 'Drop your Wea..'
+        for a in self.rates:
+            if type(a) == list and a[0] == x:
+                a[2] = a[2] + change_amt
+                assert a[2] >= 0
+                return a[1]
+        raise KeyError(x)
+    
+    def read_history(self):
+        history = self.sections[3+self.ox]
 
-shutil.copy('iba.txt', 'iba.txt.old')
-open('iba.txt', 'w').write(DELIM.join(sections) + '\n')
+        monday = datetime.datetime.utcnow()
+        monday = (monday - datetime.timedelta(days=monday.weekday())).replace(hour=0, minute=0, second=0, microsecond=0)
+
+        datefmt = '%d %B %Y %H:%M:%S'
+        hist = ''
+        when = None
+        for line in history.split('\n'):
+            if line.startswith('*'): continue
+            a, b = line[:27].strip(), line[27:].strip()
+            if a != '' and a != 'History:' and '[' not in a:
+                when = datetime.datetime.strptime(a, datefmt)
+            if b != '' and when >= monday:
+                hist += b + '\n' 
+        hist = hist.replace(',\n', ', ')
+        hist = hist.replace(' +', '\n+')
+        hist = hist.replace(' -', '\n-')
+        prev = {}
+        for line in hist.split('\n'):
+            line = line.strip()
+            if line == '' or '[' in line or ']' in line: continue
+            if line[0] == '+':
+                m = re.search('\((.*)\)', line)
+                if not m: continue
+                for n in m.group(1).split(','):
+                    n = n.strip().split('*')
+                    if len(n) > 1:
+                        p = int(n[0])
+                    else:
+                        p = 1
+                    prev[actor] = prev.get(actor, 0) + p
+
+            else:
+                actor = line
+
+        self.history = history
+        self.prev = prev
+
+    def write_history(self):
+        self.sections[3+self.ox] = self.history
+
+    def read_contract(self):
+        self.contract = self.sections[4+self.ox]
+
+    def test_retrieve_now(self):
+        # Make it actually now.  For testing.
+        self.now = datetime.datetime.utcnow()
+
+    def retrieve_now(self):
+        vr = sys.stdin.read()
+        lines = vr.split('\n')
+        dt = [lines[i+1] for i in xrange(len(lines)) if lines[i].find('by yzma.clarkk.net') != -1 or lines[i].find('by pzk37') != -1][0]
+        self.now = datetime.datetime.utcfromtimestamp(email.Utils.mktime_tz(email.Utils.parsedate_tz(dt[dt.index(';')+2:])))
+    
+
+    def transact(self, typ, asts, sorted_mode=None): # typ is 'deposit'/'withdraw'
+        total = 0
+        sore = []
+        stuff = []
+        for ast in asts.split(','):
+            ii = 1
+            if '*' in ast:
+                ast = ast.split('*')
+                ii, ast = int(ast[0]), ast[1].strip()
+            stuff.append((ii, ast))
+        for ii, ast in (sorted(stuff, key=lambda (a, b): b) if sorted_mode else stuff):
+            if ii > 1:
+                sore.append('%d*%s' % (ii, abbrevs.get(ast, ast)))
+            else:
+                sore.append(abbrevs.get(ast, ast))
+            ast = abbrevs2.get(ast, ast)
+            for i in xrange(ii):
+                if typ == 'deposit':
+                    p = self.prev.get(self.person, 0)
+                    if p >= 21:
+                        rmul = 0
+                    else:
+                        rmul = self.prev2rate[p]
+                    self.prev[self.person] = p + 1
+                else:
+                    rmul = -1
+                erate = round(rmul * self.lookup_rate(ast, 1 if typ == 'deposit' else -1))
+                self.holdings[self.person] = self.holdings.get(self.person, 0) + int(erate)
+                total += int(erate)
+                if self.holdings[self.person] < 0: raise ValueError(self.holdings[self.person])
+        total = ('+' * (typ == 'deposit')) + str(total) + 'zm'
+        self.totals.append((total, sore))
+
+    def finish_transactions(self):
+        sores = []
+        max_total_length = max(len(a[0]) for a in self.totals)
+        for total, sore in self.totals:
+            total = (' ' * (max_total_length - len(total))) + total
+            b = 28 + len(self.person) + 1 + len(total) + 2
+            c = ' ' * b
+            sores.append(total + ' (' + wrap(', '.join(sore), 70 - b).replace('\n', '\n' + c) + ')')
+
+
+        if len(sores) > 0:
+            hdr = self.now.strftime(datefmt).ljust(27) + self.person + ' '
+            hdr2 = '\n' + ' ' * len(hdr)
+            self.history += '\n' + hdr + hdr2.join(sores)
+
+    
+    def export(self):
+        return self.DELIM.join(self.sections) + '\n'
+
+
+
+def main_agi():
+    report = iba_report()
+    report.read_all()
+
+    report.test_retrieve_now()
+
+    report.person = raw_input('Person> ')
+    while True:
+        line = raw_input()
+        if line == '': break
+        report.transact(*line.strip().split())
+
+
+    report.write_all()
+
+    export = report.export()
+    print export
+    #shutil.copy('iba.txt', 'iba.txt.old')
+    #open('iba.txt', 'w').write(export)
+
+def main_agdump():
+    import json
+    report = iba_report()
+    report.read_all()
+    contract = report.contract
+    contract = re.sub(re.compile('^([XIV]+\. .*)$', re.M), '<b>\\1</b>', contract)
+    json.dump((report.rates, report.prev, sorted(report.holdings.keys(), key=str.lower), sorted(report.rdict.keys(), key=lambda a: -report.rdict[a]), contract, report.holdings), sys.stdout)
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.set_defaults(mode="agi")
+    parser.add_option("--agi", default=True, action="store_const", dest="mode", const="agi")
+    parser.add_option("--agdump", default=True, action="store_const", dest="mode", const="agdump")
+    
+    options, args = parser.parse_args()
+    {'agi': main_agi, 'agdump': main_agdump}[options.mode]()
